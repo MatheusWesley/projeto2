2ad7570b2452ae2bc0013178bdfcd1af80043d55
MatheusWesley <matheus.tarra@gmail.com>
2017-08-05 16:46:20 -0300

    Coloquei a função some em operação no main

--- main.c
+++ main.c
@@ -6,12 +6,14 @@ int main(){
   int n1=0, n2=0, r=0;
 
    printf("Ola este é uma calculadora que apenas soma dois numeros inteiros!\n");
-   printf("Primeiro numero\n");
+   printf("Primeiro numero: ");
    scanf("%d", &n1);
-   printf("Segundo numero\n");
+   printf("Segundo numero: ");
    scanf("%d", &n2);
 
    //Chamando a função soma
+   r = soma(n1,n2);
+   printf("O Resultado da soma é: %d\n", r);
 
  return 0;
 }
