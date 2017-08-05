110677ac0b73c7ea6559030df976c718dec40489
MatheusWesley <matheus.tarra@gmail.com>
2017-08-05 16:28:51 -0300

    Arquivo principal da calculadora que apenas soma dois numeros

---
+++ main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+  // Declarando variaveis
+  int n1, n2;
+
+   printf("Ola este é uma calculadora que apenas soma dois numeros inteiros!\n");
+   printf("Primeiro numero\n");
+   scanf("%d", &n1);
+   printf("Segundo numero\n");
+   scanf("%d", &n2);
+
+ return 0;
+}
