d281f489346ae676bfb66369a4c9071bc880f3d1
MatheusWesley <matheus.tarra@gmail.com>
2017-08-05 16:37:16 -0300

    Implementado a opção do segundo numero a ser adicionado

--- main.c
+++ main.c
@@ -1,14 +1,17 @@
 #include<stdio.h>
+#include "operacao.h"
 
 int main(){
   // Declarando variaveis
-  int n1, n2;
+  int n1=0, n2=0, r=0;
 
    printf("Ola este é uma calculadora que apenas soma dois numeros inteiros!\n");
    printf("Primeiro numero\n");
    scanf("%d", &n1);
    printf("Segundo numero\n");
    scanf("%d", &n2);
+
+   //Chamando a função soma
 
  return 0;
 }
